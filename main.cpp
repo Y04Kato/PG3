f164e51978f3f2105f26e0a4aa2d2e58b2ad18b4
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-10-02 14:11:49 +0900

    再帰関数の完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
-int main() {
-	
+int Normal(int n) {
+	return (n * 1072);
+}
 
+int Recursive(int n) {
+	if (n <= 1) {
+		return (100);
+	}
 
+	return (Recursive(n - 1) * 2 - 50);
+}
+
+int main(void) {
+	int hour = 10;
+
+	for (int i = 1; i < hour; i++) {
+
+		int normalResult = 0;
+		int recursiveResult = 0;
+
+		normalResult = Normal(i);
+		recursiveResult = Recursive(i);
+
+		printf("一般的な賃金：%d時間働くと%d円\n", i, normalResult);
+		printf("再帰的な賃金：%d時間働くと%d円\n\n", i, recursiveResult);
+
+	}
 	return 0;
 }
