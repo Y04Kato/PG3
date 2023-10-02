0578b6283f30ec9f2110f4a53455ced2fed4542f
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-10-02 13:45:28 +0900

    確認課題要件完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+template <typename Type>
+void Min(Type a, Type b) {
+	Type num = a < b ? a : b;
+	std::cout << num << std::endl;
+}
+
+template <>
+void Min<char>(char a, char b) {
+	std::cout << "”ŽšˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ" << std::endl;
+}
 
 int main() {
-	
 
+	Min(114, 514);
+	Min(1.0f, 0.5f);
+	Min(2.17, 8.02);
+	Min('a', 'b');
 
 	return 0;
 }
