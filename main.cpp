d5a2ea8aaf4e3e3d2aa358bd8bf4730a6e119b2e
Y04Kato <k022g0046@g.neec.ac.jp>
2023-10-23 11:08:44 +0900

    テンプレート作成

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+template <typename Type>
+
+class ClassName {
+public:
+	Type a;
+	Type b;
+
+	Type ClassName(Type a, Type b) :a(a), b(b) {};
+
+	Type Min() {
+		return static_cast<Type>(a + b);
+	}
+
+};
+
 int main() {
 	
 
