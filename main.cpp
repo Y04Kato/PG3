e5034895cb291782bf0990223fe0ca5417c6b326
Y04Kato <k022g0046@g.neec.ac.jp>
2023-10-23 11:34:35 +0900

    02_02完成

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <stdio.h>
 
-template <typename Type>
+template <typename Type1, typename Type2>
 
-class ClassName {
+class Comparison {
 public:
-	Type a;
-	Type b;
+	Type1 num1;
+	Type2 num2;
 
-	Type ClassName(Type a, Type b) :a(a), b(b) {};
+	Comparison(Type1 num1, Type2 num2) :num1(num1), num2(num2) {};
 
-	Type Min() {
-		return static_cast<Type>(a + b);
+	Type1 Min() {
+		Type1 num = num1 < num2 ? num1 : num2;
+		return static_cast<Type1>(num);
 	}
 
 };
 
 int main() {
-	
+	Comparison<int, int> a1(28, 82);
+	Comparison<int, float> a2(114, 514.0f);
+	Comparison<int, double> a3(217, 8.02);
 
+	Comparison<float, float> b1(32.0f, 14.0f);
+	Comparison<float, double> b2(7.0f, 1.42);
+
+	Comparison<double, double> c1(2.02, 4.03);
+
+	printf("%d\n", a1.Min());
+	printf("%d\n", a2.Min());
+	printf("%d\n", a3.Min());
+	
+	printf("%f\n", b1.Min());
+	printf("%f\n", b2.Min());
+	
+	printf("%lf\n", c1.Min());
 
 	return 0;
 }
