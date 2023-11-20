74e1395d117c18dbc1d2525701329b0ecb60427a
Y04Kato <k022g0046@g.neec.ac.jp>
2023-11-20 10:13:02 +0900

    新しい課題条件に沿って修正

--- main.cpp
+++ main.cpp
@@ -2,55 +2,51 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <functional>
+#include <time.h>
 
 int main(void) {
+	srand((unsigned int)time(nullptr));
 	int inputNum;
 	int sleepTime = 3;
 
-	std::function<int(int)> timeout = [](int a) {
-		for (int i = 0; i < a; i++) {
-			printf("・");
-			Sleep(1000);//1秒待つ
-		}
-
-		printf("\n");
-
-		return 0;
+	std::function<int()> randDice = []() {
+		return  rand() % 6 + 1;
 	};
 
-	std::function<int(int)>gambling = [](int a) {
-		int result = rand() % 6 + 1;
-		if (a == 2) {
+	std::function<void(int, int, int)> setTimeOut = [&](int diceResult, int inputNum, int waitTime) {
+		for (int i = 0; i < waitTime; i++) {
+			printf("・");
+			Sleep(1000);
+		}
+		printf("\n");
+
+		if (inputNum == 2) {
 			printf("貴方が選んだのは丁(偶数)です\n");
-			if (result % 2 == 0) {
-				printf("出目は%dで貴方の勝ちです\n\n", result);
+			if (diceResult % 2 == 0) {
+				printf("出目は%dで貴方の勝ちです\n\n", diceResult);
 			}
 			else {
-				printf("出目は%dで貴方の負けです\n\n", result);
+				printf("出目は%dで貴方の負けです\n\n", diceResult);
 			}
 		}
-		else if (a == 1) {
+		else if (inputNum == 1) {
 			printf("貴方が選んだのは半(奇数)です\n");
-			if ((result & 1) == 1) {
-				printf("出目は%dで貴方の勝ちです\n\n", result);
+			if ((diceResult & 1) == 1) {
+				printf("出目は%dで貴方の勝ちです\n\n", diceResult);
 			}
 			else {
-				printf("出目は%dで貴方の負けです\n\n", result);
+				printf("出目は%dで貴方の負けです\n\n", diceResult);
 			}
 		}
 		else {
 			printf("1か2を入力して下さい\n");
 		}
-
-		return 0;
 	};
 
 	while (1) {
 		printf("半(奇数)なら1、丁(偶数)なら2を入力してください\n");
 		scanf_s("%d", &inputNum);
-
-		timeout(sleepTime);
-		gambling(inputNum);
+		setTimeOut(randDice(), inputNum, 3);
 	}
 
 	return 0;
