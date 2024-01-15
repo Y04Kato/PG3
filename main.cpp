a27e3c1d0bd264de9b9ed3768c5a2ff63381846e
Y04Kato <k022g0046@g.neec.ac.jp>
2024-01-15 11:04:05 +0900

    06_02完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <thread>
+
+void PrintThread(int num) {
+	printf("thread %d\n", num);
+}
 
 int main() {
+	std::thread th1(PrintThread, 1);
+	th1.join();
+
+	std::thread th2(PrintThread, 2);
+	th2.join();
+
+	std::thread th3(PrintThread, 3);
+	th3.join();
 
 	return 0;
 }
