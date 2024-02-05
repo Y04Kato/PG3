472683cc4cd0333a53e994d2d9b93df33c48c9f7
Y04Kato <k022g0046@g.neec.ac.jp>
2024-02-05 10:20:29 +0900

    07_03完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <Windows.h>
 
 int main() {
+    std::string a(100000, 'a');
 
-	return 0;
+    auto start = std::chrono::steady_clock::now();
+    std::string copy = a;
+    auto end = std::chrono::steady_clock::now();
+
+    auto duration = end - start;
+    std::cout << "コピーにかかった時間 : " << duration.count() << " マイクロ秒" << std::endl;
+
+
+    start = std::chrono::steady_clock::now();
+    std::string move = std::move(a);
+    end = std::chrono::steady_clock::now();
+
+    duration = end - start;
+    std::cout << "移動にかかった時間 : " << duration.count() << " マイクロ秒" << std::endl;
+
+    return 0;
 }
